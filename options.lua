2132c198afb6a4b14bd1f8a541a193168a33eac8
Sergey Naumov <drmgc@yandex.ru>
2024-03-12 15:00:51 +0500

    `set bg=light` by default

--- options.lua
+++ options.lua
@@ -8,6 +8,7 @@ return {
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = true, -- sets vim.opt.wrap
     guifont = "Hack_Nerd_Font_Mono:h16",
+    background = "light",
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
